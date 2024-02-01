d73376cfd489c6a404fbba4a5414d1014908b57e
DeiMoS <nagib.86@gmail.com>
2024-02-01 09:27:11 +0100

    update

--- 002-porfolio-nagib/astro.config.mjs
+++ 002-porfolio-nagib/astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwind(), 
     partytown({
       config: {
-          forward: ['dataLayer.push']
+          forward: ['datalayer.push']
       }
     })
   ]
